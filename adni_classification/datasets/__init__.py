285374269c971191f6c224568c0da50362441b82
Tin-Hoang <tin@cinnamon.is>
2025-04-19 16:17:31 +0100

    fix

--- adni_classification/datasets/__init__.py
+++ adni_classification/datasets/__init__.py
@@ -1,7 +1,6 @@
 """Data package for ADNI classification."""
 
 from adni_classification.datasets.adni_smartcache_dataset import ADNIDataset as ADNISmartCacheDataset
-from adni_classification.datasets.adni_smartcache_dataset import get_multiprocessing_transforms
 from adni_classification.datasets.transforms import get_transforms
 from adni_classification.datasets.adni_cache_dataset import ADNIDataset as ADNICacheDataset
 from adni_classification.datasets.dataset_factory import create_adni_dataset, get_transforms_from_config
@@ -10,7 +9,6 @@ __all__ = [
     "ADNISmartCacheDataset",
     "ADNICacheDataset",
     "get_transforms",
-    "get_multiprocessing_transforms",
     "create_adni_dataset",
     "get_transforms_from_config"
 ]
